ed6afa2ba191bf94d6a312e8972255355af9152d
Dionisi Lola <dionisislolas@gmail.com>
2024-10-22 12:00:39 +0100

    Add login template page

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -3,17 +3,32 @@ package handlers
 import (
 	"context"
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/ONSdigital/dis-authentication-stub/utils"
+	"github.com/ONSdigital/log.go/v2/log"
 )
 
 func FlorenceLoginHandler(ctx context.Context) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello Florence login handler....")
+		if req.Method != http.MethodGet {
+			http.Error(w, "Request method not allowed", http.StatusMethodNotAllowed)
+		}
+
 		users := utils.LoadUsers(ctx, "static/json/users.json")
-		if len(users) > 0 {
-			fmt.Println("Users loaded...")
+
+		filename := "templates/user.login.html"
+		tmpl, err := template.ParseFiles(filename)
+		if err != nil {
+			log.Fatal(ctx, fmt.Sprintf("could not parse template file %s", filename), err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		err = tmpl.Execute(w, users)
+		if err != nil {
+			log.Fatal(ctx, "could not apply template", err)
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	}
 }
