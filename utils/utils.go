921a8616f3495f827f298e6046b0928414394ea8
Dionisi Lola <dionisislolas@gmail.com>
2024-10-22 14:38:40 +0100

    Refactor function to return an error

--- utils/utils.go
+++ utils/utils.go
@@ -11,17 +11,19 @@ import (
 	"github.com/ONSdigital/log.go/v2/log"
 )
 
-func LoadUsers(ctx context.Context, filename string) []models.User {
+func LoadUsers(ctx context.Context, filename string) ([]models.User, error) {
 	fmt.Println("In LoadUsers...")
 	usersJsonFile, err := os.Open(filename)
 	if err != nil {
 		log.Fatal(ctx, fmt.Sprintf("could not open file %s", filename), err)
+		return nil, err
 	}
 	defer usersJsonFile.Close()
 
 	usersJsonFileBytes, err := io.ReadAll(usersJsonFile)
 	if err != nil {
 		log.Fatal(ctx, fmt.Sprintf("could not read from file %s", filename), err)
+		return nil, err
 	}
 
 	var users []models.User
@@ -29,7 +31,8 @@ func LoadUsers(ctx context.Context, filename string) []models.User {
 	err = json.Unmarshal(usersJsonFileBytes, &users)
 	if err != nil {
 		log.Fatal(ctx, fmt.Sprintf("could not unmarshal JSON from file %s", filename), err)
+		return nil, err
 	}
 
-	return users
+	return users, nil
 }
