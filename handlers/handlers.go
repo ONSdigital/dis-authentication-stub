05616e274261a17e10a9335492c049588fed495a
Dionisi Lola <dionisislolas@gmail.com>
2024-10-22 16:19:01 +0100

    Process redirect url in the login handler

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -21,6 +21,11 @@ func FlorenceLoginHandler(ctx context.Context) http.HandlerFunc {
 			http.Error(w, "Request method not allowed", http.StatusMethodNotAllowed)
 		}
 
+		redirectURL := req.URL.Query().Get("redirect")
+		if redirectURL == "" {
+			redirectURL = "/florence/collections"
+		}
+
 		users, err := utils.LoadUsers(ctx, "static/json/users.json")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -35,7 +40,12 @@ func FlorenceLoginHandler(ctx context.Context) http.HandlerFunc {
 			return
 		}
 
-		err = tmpl.Execute(w, users)
+		var data = models.TemplateData{
+			Users:       users,
+			RedirectURL: redirectURL,
+		}
+
+		err = tmpl.Execute(w, data)
 		if err != nil {
 			log.Fatal(ctx, "could not apply template", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
