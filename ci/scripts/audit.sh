bba1f09e52da174a4934150ac1bdedda70051161
aryan-patel <aryanpatel1771@gmail.com>
2024-11-11 08:39:17 +0000

    Removed api, ci and feature folders

--- ci/scripts/audit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -eux
-
-pushd dis-authentication-stub
-  make audit
-popd
--- ci/scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -eux
-
-pushd dis-authentication-stub
-  make build
-  cp build/dis-authentication-stub Dockerfile.concourse ../build
-popd
