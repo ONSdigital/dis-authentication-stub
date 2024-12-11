0f5fac711cb5a1f10a98b2865985f7d55f5626ac
Nick Lupton <19624419+mr-nick17@users.noreply.github.com>
2024-12-11 10:27:22 +0000

    2088 update linter to latest version

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eux
 
 pushd dis-authentication-stub
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.61.0
+  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
   make lint
 popd
