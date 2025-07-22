c470d42938942b63f1c5db96876910ba8de5045a
lindenmckenzie <linden.mckenzie@ons.gov.uk>
2025-07-22 06:26:38 +0100

    Upgrade linter to v2

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -eux
 
 pushd dis-authentication-stub
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.62.2
   make lint
 popd
--- models/constants.go
+++ models/constants.go
@@ -5,5 +5,6 @@ const (
 	RefreshTokenCookie = "refresh_token"
 	IDTokenCookie      = "id_token"
 
+	//nolint:gosec // This is a path, not a credential
 	RefreshTokenCookiePath = "/api/v1/tokens/self"
 )
